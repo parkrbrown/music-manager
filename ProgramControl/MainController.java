070a5f829fa0fbf14a8ee6c60faa7fd458694e6e
Scott Fries <sfries@student.neumont.edu>
2014-02-27 12:12:51 -0700

    GUI Elements Declared & Initialized in MainController

--- ProgramControl/MainController.java
+++ ProgramControl/MainController.java
@@ -1,9 +1,13 @@
 package ProgramControl;
 
 import GUI.FileFormatSelectionWindow;
+import GUI.MainMenu;
+import GUI.OganizationStyleSelectionWindow;
+import GUI.TagEditWindow;
 
 public class MainController {
     MainMenu MainMenu = new MainMenu();
-	FileFormatSelectionWindow FFSW = new FileFormatSelectionWindow();
-    
+	OganizationStyleSelectionWindow OSSW = new OganizationStyleSelectionWindow();
+    FileFormatSelectionWindow FFSW = new FileFormatSelectionWindow();
+    TagEditWindow TEW = new TagEditWindow();
 }
