03cf8b4f2a72d9ec960b7372890e1328d0c3e368
Scott Fries <sfries@student.neumont.edu>
2014-03-03 10:32:56 -0700

    Mp3 Model Created

--- ProgramControl/MainController.java
+++ ProgramControl/MainController.java
@@ -1,10 +1,17 @@
 package ProgramControl;
 
-import GUI.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import GUI.FileFormatSelectionWindow;
+import GUI.MainMenu;
+import GUI.OganizationStyleSelectionWindow;
+import GUI.TagEditWindow;
 
 public class MainController {
     MainMenu MainMenu = new MainMenu();
     OganizationStyleSelectionWindow OSSW = new OganizationStyleSelectionWindow();
     FileFormatSelectionWindow FFSW = new FileFormatSelectionWindow();
     TagEditWindow TEW = new TagEditWindow();
+    
+    public FileNameExtensionFilter FileFilter = new FileNameExtensionFilter("MP3 Music Files", "mp3");
 }
---
+++ ProgramModels/MP3.java
@@ -0,0 +1,43 @@
+package ProgramModels;
+
+import java.io.File;
+import java.io.IOException;
+
+import mp3agic.ID3v1;
+import mp3agic.ID3v2;
+import mp3agic.InvalidDataException;
+import mp3agic.Mp3File;
+import mp3agic.UnsupportedTagException;
+
+public class MP3
+{
+	Mp3File mp3;
+	ID3v1 ID3v1Tag;
+	ID3v2 ID3v2Tag;
+	
+	public MP3(File MP3Location)
+	{
+		try
+		{
+			mp3 = new Mp3File(MP3Location.toString());
+		}
+		catch (UnsupportedTagException | InvalidDataException | IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if(this.mp3.hasId3v1Tag())
+		{
+			this.ID3v1Tag = mp3.getId3v1Tag();
+		}
+		else if(this.mp3.hasId3v2Tag())
+		{
+			this.ID3v2Tag = mp3.getId3v2Tag();
+		}
+		else
+		{
+			//TODO Error out with no ID3 tag || Create ID3 Tag?
+		}
+	}
+}
