c6d2a154c93f995f63406fa9eefa6b4a0bdba88f
Parker Brown <parkrbrown@gmail.com>
2014-03-03 10:44:11 -0700

    Added main method to MainController

--- ProgramControl/MainController.java
+++ ProgramControl/MainController.java
@@ -14,4 +14,14 @@ public class MainController {
     TagEditWindow TEW = new TagEditWindow();
     
     public FileNameExtensionFilter FileFilter = new FileNameExtensionFilter("MP3 Music Files", "mp3");
+    
+    public static void main (String[] args){
+    	runApp();
+    }
+    
+    private static void runApp(){
+    	// start program
+    	// display main window
+    	// wait for selections
+    }
 }
