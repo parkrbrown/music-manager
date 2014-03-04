3519f41d9ea356e28e26690ccbc0109365b74366
Scott Crisenbery <scrisenbery@student.neumont.edu>
2014-03-04 10:22:22 -0700

    Added Function Classes
    These classes implement FilController

---
+++ ProgramControl/TagEditor.java
@@ -0,0 +1,33 @@
+package ProgramControl;
+
+import java.io.IOException;
+
+import mp3agic.*;
+
+public class TagEditor implements FileController {
+
+	public TagEditor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void updateDirectory(String directory) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void saveFile(String file) throws UnsupportedTagException,
+			InvalidDataException, IOException, NotSupportedException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeTags(String file) throws UnsupportedTagException,
+			InvalidDataException, IOException, NotSupportedException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
