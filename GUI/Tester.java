975f0d424421bdf5643ad84fa87c60a7f04ff561
Scott Fries <sfries@student.neumont.edu>
2014-03-06 13:47:23 -0700

    .

--- GUI/Tester.java
+++ GUI/Tester.java
@@ -1,13 +1,21 @@
 package GUI;
 
+import mp3agic.Mp3File;
+
 public class Tester
 {
 	public static void main (String[] args)
 	{
-		OganizationStyleSelectionWindow OS = new OganizationStyleSelectionWindow();
-		OS.setVisible(true);
+//		OganizationStyleSelectionWindow OS = new OganizationStyleSelectionWindow();
+//		OS.setVisible(true);
 		
 //		TagEditWindow TEW = new TagEditWindow();
 //		TEW.setVisible(true);
+		
+		MainMenu MM = new MainMenu();
+		MM.setVisible(true);
+		
+		Mp3File mp3 = new Mp3File();
+		mp3.getId3v2Tag().set
     }
 }
