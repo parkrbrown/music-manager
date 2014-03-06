0255f224fd26facc5d657f6e7e41c8309b433046
Scott Fries <sfries@student.neumont.edu>
2014-03-06 11:11:33 -0700

    Fixed Launch Directory Issue

--- ProgramControl/MainController.java
+++ ProgramControl/MainController.java
@@ -13,18 +13,18 @@ import GUI.TagEditWindow;
 
 
 public class MainController {
-	//GUI Windows for Program
-	public static MainMenu MainMenu = new MainMenu();
-	public static OganizationStyleSelectionWindow OSSW = new OganizationStyleSelectionWindow();
-	public static FileFormatSelectionWindow FFSW = new FileFormatSelectionWindow();
-	public static TagEditWindow TEW = new TagEditWindow();
-	public static JFrame ReferenceFrame = new JFrame();
-    
 	//File Designation variables for Program
-    public static File CurrentDirectory;
+    public static File CurrentDirectory = new File("Libraries\\Music");
     public static JFileChooser FileChooser = new JFileChooser();
  /*TODO Probably need a different class for limiting files*/
     public static FileNameExtensionFilter FileFilter = new FileNameExtensionFilter("MP3 Music Files", "mp3");
+    
+    //GUI Windows for Program
+  	public static MainMenu MainMenu = new MainMenu();
+  	public static OganizationStyleSelectionWindow OSSW = new OganizationStyleSelectionWindow();
+  	public static FileFormatSelectionWindow FFSW = new FileFormatSelectionWindow();
+  	public static TagEditWindow TEW = new TagEditWindow();
+  	public static JFrame ReferenceFrame = new JFrame();
     
     
 	//TODO
