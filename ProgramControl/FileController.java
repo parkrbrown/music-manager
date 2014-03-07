8ddb07aca343816ca5f3f443b85f20935b6cca9c
Scott Crisenbery <scrisenbery@student.neumont.edu>
2014-03-07 10:19:00 -0700

    Methods and Descriptions added to FileController

--- ProgramControl/FileController.java
+++ ProgramControl/FileController.java
@@ -5,15 +5,24 @@ import java.io.IOException;
 import mp3agic.*;
 
 public class FileController {	
-	void findTagVersion(Mp3File MP3) {
+	void checkForTag(Mp3File mp3) {
+		//Checks for a v1 tag and creates a new one if none is found
 		// TODO
 	}
 	
+	ID3v1Tag getTag(Mp3File mp3) {
+		//returns actual ID3v1 tag
+		return null;
+		//TODO
+	}
+	
     void saveFile(String file) throws UnsupportedTagException, InvalidDataException, IOException, NotSupportedException {
+    	//Saves file to where it was before
     	//TODO
     }
     
     void findMP3s() {
+    	//Derek has this; pulls the list of mp3 files in the directory
     	//TODO
     }
 }
