5bfa8ee13c4afb2f20b48e2bc850521ddfb5f189
Scott Crisenbery <scrisenbery@student.neumont.edu>
2014-03-07 11:50:08 -0700

    Began TagEditor Method

--- ProgramControl/TagEditor.java
+++ ProgramControl/TagEditor.java
@@ -1,17 +1,69 @@
 package ProgramControl;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import mp3agic.*;
 
 public class TagEditor extends FileController {
+	private ArrayList<Mp3File> fileList;
+	String newArtist;
+	String newAlbum;
+	String newGenre;
 
 	public TagEditor() {
 		// TODO Auto-generated constructor stub
 	}
 	
 	public void updateTags() {
-		//pulls all data directly from menu
+		//VARIABLES
+		
+		fileList = findMP3s();
+		
+		if (MainController.TEW.newArtist() == true) {
+			newArtist = MainController.TEW.getNewArtist();
+		}
+		else {
+			newArtist = null;
+		}
+		
+		if (MainController.TEW.newAlbum() == true) {
+			newAlbum = MainController.TEW.getNewAlbum();
+		}
+		else {
+			newAlbum = null;
+		}
+		
+		if (MainController.TEW.newGenre() == true) {
+			newGenre = MainController.TEW.getNewGenre();
+		}
+		else {
+			newGenre = null;
+		}
+		
+		
+		for (int index = 0; index < fileList.size(); index++) {
+			//Checks for tag and creates one if necessary
+			checkForTag(fileList.get(index));
+			//Gets the tag from the file
+			ID3v1Tag thisTag = getTag(fileList.get(index));
+			
+			if (newArtist != null) {
+				thisTag.setArtist(newArtist);
+			}
+			
+			if (newAlbum != null) {
+				thisTag.setAlbum(newAlbum);
+			}
+			
+			if (newGenre != null) {
+				thisTag.setGenre(newGenre);
+			}
+			
+		}
+		
+		
 	}
 
 }
