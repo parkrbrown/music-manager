493a8da82fcb1dd2ce992480aa8cc8c85bc2fbd5
Derek Nasfell <dnasfell@student.neumont.edu>
2014-03-10 13:59:04 -0600

    FilenameFormatter
    Progress on format();

--- ProgramControl/FilenameFormatter.java
+++ ProgramControl/FilenameFormatter.java
@@ -2,6 +2,7 @@ package ProgramControl;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.activation.MailcapCommandMap;
 
@@ -38,10 +39,67 @@ public class FilenameFormatter extends FileController {
 		
 		//Derek is working here...
 		
-		String directoryName = MainController.CurrentDirectory.getAbsolutePath();
-		findMP3s(directoryName);
-		MainController.FFSW.getFormatOrder();
+		String currentDirectory = MainController.CurrentDirectory.getAbsolutePath();
+//		String saveDirectory = MainController.CurrentDirectory.getAbsolutePath();
+		ArrayList<File> mp3s = findMP3s(currentDirectory);
+		ArrayList<String> renameTo = MainController.FFSW.getFormatOrder();
 		
+		//for each song
+			//rename to match getFormatOrder();
+		for(int i = 0; i < mp3s.size(); i++){
+			Mp3File mp3;
+			try {
+				mp3 = new Mp3File(mp3s.get(i).getAbsolutePath());
+			} catch (UnsupportedTagException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvalidDataException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			StringBuffer NewName = new StringBuffer();
+			for(int j = 0; j < renameTo.size(); j++)
+			{
+				//album, artist, <year, genre,>?? song title
+				if(renameTo.get(j) == "Title")
+				{
+					NewName.append(mp3.getId3v1Tag().getTitle());
+					NewName.append(" - ");
+				}
+				
+				else if(renameTo.get(j) == "Album")
+				{
+					NewName.append(mp3.getId3v1Tag().getAlbum());
+					NewName.append(" - ");
+				}
+				
+				else if(renameTo.get(j) == "Artist")
+				{
+					NewName.append(mp3.getId3v1Tag().getArtist());
+					NewName.append(" - ");
+				}
+				
+				else if(renameTo.get(j) == "Year")
+				{
+					NewName.append(mp3.getId3v1Tag().getYear());
+					NewName.append(" - ");
+				}
+				
+				else if(renameTo.get(j) == "Genre")
+				{
+					NewName.append(mp3.getId3v1Tag().getGenreDescription());
+					NewName.append(" - ");
+				}
+			}
+			NewName.delete((NewName.length() - 3), NewName.length());
+			String NewFileName = currentDirectory + NewName.toString() + ".mp3";
+			
+			MainController.FileController.saveFile((new File(mp3s.get(i).getAbsolutePath())), (new File(NewFileName)), mp3);
+		}
 		
 		
 		}
