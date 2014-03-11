614bc06b429e7a23e7448b55919208a6a6d368ad
Parker Brown <parkrbrown@gmail.com>
2014-03-11 17:14:48 -0600

    Javadocs & Formatting

--- ProgramControl/MainController.java
+++ ProgramControl/MainController.java
@@ -11,32 +11,32 @@ import GUI.MainMenu;
 import GUI.OrganizationStyleSelectionWindow;
 import GUI.TagEditWindow;
 
-
+/**
+ * The MainController
+ */
 public class MainController {
-	//File Designation variables for Program
+
+    //File Designation variables for Program
     public static File CurrentDirectory = new File("Libraries\\Music");
     public static JFileChooser FileChooser = new JFileChooser();
- /*TODO Probably need a different class for limiting files*/
+    /*TODO Probably need a different class for limiting files*/
     public static FileNameExtensionFilter FileFilter = new FileNameExtensionFilter("MP3 Music Files", "mp3");
-    
+
     //GUI Windows for Program
-  	public static MainMenu MainMenu = new MainMenu();
-  	public static OrganizationStyleSelectionWindow OSSW = new OrganizationStyleSelectionWindow();
-  	public static FileFormatSelectionWindow FFSW = new FileFormatSelectionWindow();
-  	public static TagEditWindow TEW = new TagEditWindow();
-  	public static JFrame ReferenceFrame = new JFrame();
-    
-    
-	//File Controllers for Program
-  	public static FileController FileController = new FileController();
-  	public static FilenameFormatter FNF = new FilenameFormatter();
-  	public static FileOrganizer FO = new FileOrganizer();
-  	public static TagEditor TE = new TagEditor();
-	
-	
-	
-    public static void main (String[] args) {
-    	FileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-    	MainMenu.setVisible(true);
+    public static MainMenu MainMenu = new MainMenu();
+    public static OrganizationStyleSelectionWindow OSSW = new OrganizationStyleSelectionWindow();
+    public static FileFormatSelectionWindow FFSW = new FileFormatSelectionWindow();
+    public static TagEditWindow TEW = new TagEditWindow();
+    public static JFrame ReferenceFrame = new JFrame();
+
+    //File Controllers for Program
+    public static FileController FileController = new FileController();
+    public static FilenameFormatter FNF = new FilenameFormatter();
+    public static FileOrganizer FO = new FileOrganizer();
+    public static TagEditor TE = new TagEditor();
+
+    public static void main(String[] args) {
+        FileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        MainMenu.setVisible(true);
     }
 }
--- ProgramControl/TagEditor.java
+++ ProgramControl/TagEditor.java
@@ -15,65 +15,69 @@ import mp3agic.UnsupportedTagException;
  * clicks confirm and updates the selected tags with their choices.
  */
 public class TagEditor extends FileController {
-	private ArrayList<File> fileList;
-	private ArrayList<Mp3File> mp3List;
-	private String newArtist = null;
-	private String newAlbum = null;
-	private int newGenre = -2; //Uses -2 as an equivalent for null
 
-	public TagEditor() {
-		//No tasks for constructor
-	}
-	
-	public void updateTags() {
-		//VARIABLES
-		fileList = findMP3s(); //Builds list of files that are mp3s
-		//Converts fileList into a list of Mp3Files
-		for (int count = 0; count < fileList.size(); count++) {
-			Mp3File temp = null;
-			try {
-				temp = new Mp3File(fileList.get(count).getAbsolutePath());
-			} catch (UnsupportedTagException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InvalidDataException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			mp3List.add(temp);
-		}
-		//Gets data from window
-		if (MainController.TEW.isArtistSelected() == true) {
-			newArtist = MainController.TEW.getTArtist();
-		}
-		if (MainController.TEW.isAlbumSelected() == true) {
-			newAlbum = MainController.TEW.getTAlbum();
-		}
-		if (MainController.TEW.isGenreSelected() == true) {
-			newGenre = MainController.TEW.getSelectedGenre();
-		}
-		
-		//ACTIONS
-		for (int index = 0; index < mp3List.size(); index++) {
-			//Checks for tag and creates one if necessary
-			checkForTag(mp3List.get(index));
-			//Gets the tag from the file
-			ID3v1Tag thisTag = getTag(mp3List.get(index));
-			//Updates tag
-			if (newArtist != null) {
-				thisTag.setArtist(newArtist);
-			}
-			if (newAlbum != null) {
-				thisTag.setAlbum(newAlbum);
-			}
-			if (newGenre != -2) {
-				thisTag.setGenre(newGenre);
-			}
-			//Saves file
-			saveFile(fileList.get(index), fileList.get(index), mp3List.get(index));
-		}
-	}
+    private ArrayList<File> fileList;
+    private ArrayList<Mp3File> mp3List;
+    private String newArtist = null;
+    private String newAlbum = null;
+    private int newGenre = -2; //Uses -2 as an equivalent for null
+
+    public TagEditor() {
+        //No tasks for constructor
+    }
+
+    /**
+     * Gets existing ID3 Tag, changes the given values, and saves the changes.
+     */
+    public void updateTags() {
+        //VARIABLES
+        fileList = findMP3s(); //Builds list of files that are mp3s
+        //Converts fileList into a list of Mp3Files
+        for (int count = 0; count < fileList.size(); count++) {
+            Mp3File temp = null;
+            try {
+                temp = new Mp3File(fileList.get(count).getAbsolutePath());
+            } catch (UnsupportedTagException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (InvalidDataException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            mp3List.add(temp);
+        }
+        //Gets data from window
+        if (MainController.TEW.isArtistSelected() == true) {
+            newArtist = MainController.TEW.getTArtist();
+        }
+        if (MainController.TEW.isAlbumSelected() == true) {
+            newAlbum = MainController.TEW.getTAlbum();
+        }
+        if (MainController.TEW.isGenreSelected() == true) {
+            newGenre = MainController.TEW.getSelectedGenre();
+        }
+
+        //ACTIONS
+        for (int index = 0; index < mp3List.size(); index++) {
+            //Checks for tag and creates one if necessary
+            checkForTag(mp3List.get(index));
+            //Gets the tag from the file
+            ID3v1Tag thisTag = getTag(mp3List.get(index));
+            //Updates tag
+            if (newArtist != null) {
+                thisTag.setArtist(newArtist);
+            }
+            if (newAlbum != null) {
+                thisTag.setAlbum(newAlbum);
+            }
+            if (newGenre != -2) {
+                thisTag.setGenre(newGenre);
+            }
+            //Saves file
+            saveFile(fileList.get(index), fileList.get(index), mp3List.get(index));
+        }
+    }
 }
