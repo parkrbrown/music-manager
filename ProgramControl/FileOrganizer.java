d10d8f4605f7f512a9cc7a99359887334b1229cd
Scott Crisenbery <scrisenbery@student.neumont.edu>
2014-03-11 11:49:09 -0600

    Began FileOrganizer Functionality
    Created organize method() (Starting method for class);
    Created findNonMP3s() method;
    Created checkForNonMP3s() method;
    Removed or commented out some unnecessary method skeletons

--- ProgramControl/FileOrganizer.java
+++ ProgramControl/FileOrganizer.java
@@ -1,58 +1,105 @@
 package ProgramControl;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import mp3agic.*;
 
 public class FileOrganizer extends FileController {
-
+	private ArrayList<File> fileList;
+	private ArrayList<File> nonMp3Files;
+	
 	public FileOrganizer() {
-		// TODO Auto-generated constructor stub
+		// No tasks for constructor
 	}
 	
-	//Some kind of starting method
-	
-	public void moveFile(/*String oldPath, String newPath*/){
-		// TODO move file from oldPath to newPath
-		// For an individual file
+	//Starting Method
+	public void organize() {
+		//Derek working here...
+		//Get cur directory and scan dir for mp3s
+		fileList = findMP3s();
+		checkForNonMP3s(); //if file !mp3 prompt user to move the file into a new folder, 
+			//re-run once files are out of the folder.  give absolute path for the dir that has non-mp3s
 		
-		//or use Files.move()
-	}		
-	
-	
-	//Derek working here...
-	//Get cur directory
-	//scan dir for mp3s
-		//if file !mp3 --- ignore directories
-			//prompt user to move the file into a new folder, re-run once files are out of the folder.  give absolute path for the dir that doens't have mp3s
-	//move all mp3s from sub directories to current dir
-	//remove empty directories
-	//create new directories IAW user preferences
-		//check if folder already exists
-			//if true
-				//don't make dir
-			//else create
-	//move files that have matching id3 tags to appropriate folder
-		//scan id3 field to match getFormatOrder().length
-			//if (field 1 matches)
-				//scan field 2;
-				//if (field 2 matches)
-				//add mp3 to array
-		//move files in array to the created directory
-	static String rootDirectory = MainController.CurrentDirectory.getAbsolutePath();
-	
-	public void findMp3s(){
+		
+		//move all mp3s from sub directories to current dir
+		moveFilesToRoot();
+		//remove empty directories
+		deleteEmptyFolders();
+		
+		//create new directories IAW user preferences
+			//check if folder already exists
+				//if true
+					//don't make dir
+				//else create
+		//move files that have matching id3 tags to appropriate folder
+			//scan id3 field to match getFormatOrder().length
+				//if (field 1 matches)
+					//scan field 2;
+					//if (field 2 matches)
+					//add mp3 to array
+			//move files in array to the created directory
+		//static String rootDirectory = MainController.CurrentDirectory.getAbsolutePath();
 		
 	}
 	
-	public void moveMp3sToRootDirectory(){
+//	public void moveFile(/*String oldPath, String newPath*/){
+//		// TODO move file from oldPath to newPath
+//		// For an individual file
+//		
+//		//or use Files.move()
+//	}
+	
+	private void checkForNonMP3s() /* TODO throws Exception */ {
+		ArrayList<File> nonMp3Files = findNonMP3s();
 		
+		if (nonMp3Files.size() > 0) {
+			//throw new nonMP3FileException
+		}
+	}
+	
+	private ArrayList<File> findNonMP3s() {
+		//Filter files by .mp3 extension
+		FilenameFilter mp3Filter = new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return !(name.endsWith(".mp3"));
+    		}
+		};
+
+		File directory = new File(MainController.CurrentDirectory.toString());
+		//Add all files in directory to an array
+		File[] fileList = directory.listFiles(); 
+		ArrayList<File> mp3Files = new ArrayList<File>();
+
+    	if(fileList == null){
+    		//TODO Give alert		System.out.println("Directory not found: " + directory);
+    	}else if(fileList.length == 0){
+    		//TODO Give alert		System.out.println("Empty directory: " + directory);
+    	}else{
+
+			//Go through all files in the fileList and add .mp3 files to the ArrayList
+			for (File file : fileList){
+				if (file.isFile()){
+					if(mp3Filter.accept(directory,file.getName())){
+						mp3Files.add(file);
+					}
+				//If a sub-directory is found, search for mp3s in that directory
+				} else if (file.isDirectory()){
+					findMP3s();
+				}
+			}
+    	}
+    	return mp3Files;
+    }
+	
+	public void moveFilesToRoot(){
+		//TODO
 	}
 	
 	public void deleteEmptyFolders(){
-		
+		//TODO
 	}
 	
 	public void createDirectories(){
